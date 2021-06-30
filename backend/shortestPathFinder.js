7236cf14830e737b46b27b672031b88ea8b78825
Owen Jones <otj202@nyu.edu>
2021-06-30 16:47:56 -0400

    create a ShortestPathFinder class

---
+++ backend/shortestPathFinder.js
@@ -0,0 +1,31 @@
+
+class ShortestPathFinder{
+    constructor(){
+        let jason=require("./map.json");
+        console.log("the json object:",jason);
+        this.map=jason;
+    }
+    getShortestPath(items){
+        
+    }
+    dijkstras(node){
+
+    }
+    searchForSortestItem(dists){
+
+    }
+}
+let p = new ShortestPathFinder();
+console.log(p);
+/* IDEAS:
+let curr=map["start"];
+let path=[curr];
+while(items.length > 0)
+{
+    dists = dijkstras(curr);//dists["chicken"] = length of shortest path from curr to chicken
+    curr = searchForShortestItem(dists);
+    items.remove(curr);
+    path.append(curr);
+}
+path.append(map["end"]);
+*/
