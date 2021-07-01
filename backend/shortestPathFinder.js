ce2adc22fca2bb86da5101f2ae733de7f0e5978a
Joe Sanders <joesande@publicisgroupe.net>
2021-07-01 11:48:00 -0400

    Completed get shortest path method of shortestPathFinder

--- backend/shortestPathFinder.js
+++ backend/shortestPathFinder.js
@@ -5,8 +5,38 @@ class ShortestPathFinder{
         console.log("the json object:",jason);
         this.map=jason;
     }
+
+    //@param items
+    //array of strings of names of items of shopping cart
     getShortestPath(items){
-        
+        //Start path on start node
+        let curItem = this.map.start; //curItem is a node
+        let path = ["start"]; //array of strings of names of items
+
+        while(items.length > 0) {
+            let dists = this.dijkstras(curItem, items);
+            nextItem = this.searchForShortestItem(dists)
+
+            let pathToNextItem = [nextItem];
+            let curPathNode = nextItem;
+            while(dists[curPathNode].parent !== curItem) {
+                curPathNode = dists[curPathNode].parent;
+                pathToNextItem.unshift(curPathNode);       
+            }   
+
+            Array.prototype.push.apply(path, pathToNextItem)
+
+            curItem = nextItem;
+
+            //remove current item from items
+            const index = items.indexOf(curItem);
+            if (index > -1) {
+                items.splice(index, 1);
+            }
+        }
+
+        path.push("exit")
+        return path;
     }
     dijkstras(node,items){
 
