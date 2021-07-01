b3bf79b737876c5042eed84e80c1bb6dd8fb71e9
Owen Jones <otj202@nyu.edu>
2021-07-01 15:38:00 -0400

    updated shortestPathFinder, map

--- backend/shortestPathFinder.js
+++ backend/shortestPathFinder.js
@@ -1,3 +1,87 @@
+class QElement{
+    constructor(element,priority){
+        this.element=element;
+        this.priority=priority;
+    }
+}
+
+class PriorityQueue{
+    constructor(){
+        this.heap=[null];
+    }
+    isEmpty(){
+        return this.heap.length == 1;
+    }
+    enqueue(node,priority){
+  // creating object from queue element
+        var qElement = new QElement(node, priority);
+        this.heap.push(qElement);
+    
+    /* Finding the correct position for the new node */
+
+        if (this.heap.length > 1) {
+            let current = this.heap.length - 1
+        
+        /* Traversing up the parent node until the current node (current) is greater than the parent (current/2)*/
+            while (current > 1 && this.heap[Math.floor(current/2)].priority > this.heap[current].priority) {
+        
+            /* Swapping the two nodes by using the ES6 destructuring syntax*/
+                [this.heap[Math.floor(current/2)], this.heap[current]] = [this.heap[current], this.heap[Math.floor(current/2)]]
+                current = Math.floor(current/2)
+            }
+        }
+    }
+
+    dequeue(){
+        let smallest = this.heap[1]
+    
+        /* When there are more than two elements in the array, we put the right most element at the first position
+            and start comparing nodes with the child nodes
+        */
+        if (this.heap.length > 2) {
+            this.heap[1] = this.heap[this.heap.length-1]
+            this.heap.splice(this.heap.length - 1)
+    
+            if (this.heap.length === 3) {
+                if (this.heap[1].priority > this.heap[2].priority) {
+                    [this.heap[1], this.heap[2]] = [this.heap[2], this.heap[1]]
+                }
+                return smallest.element;
+            }
+    
+            let current = 1
+            let leftChildIndex = current * 2
+            let rightChildIndex = current * 2 + 1
+    
+            while (this.heap[leftChildIndex] &&
+                    this.heap[rightChildIndex] &&
+                    (this.heap[current].priority > this.heap[leftChildIndex].priority ||
+                        this.heap[current].priority > this.heap[rightChildIndex].priority)) {
+                if (this.heap[leftChildIndex].priority < this.heap[rightChildIndex].priority) {
+                    [this.heap[current], this.heap[leftChildIndex]] = [this.heap[leftChildIndex], this.heap[current]]
+                    current = leftChildIndex
+                } else {
+                    [this.heap[current], this.heap[rightChildIndex]] = [this.heap[rightChildIndex], this.heap[current]]
+                    current = rightChildIndex
+                }
+    
+                leftChildIndex = current * 2
+                rightChildIndex = current * 2 + 1
+            }
+        }
+        
+        /* If there are only two elements in the array, we directly splice out the first element */
+        
+        else if (this.heap.length === 2) {
+            this.heap.splice(1, 1)
+        } else {
+            return null
+        }
+    
+        return smallest.element;
+  
+    }
+}
 
 class ShortestPathFinder{
     constructor(){
@@ -15,13 +99,13 @@ class ShortestPathFinder{
 
         while(items.length > 0) {
             let dists = this.dijkstras(curItem, items);
-            nextItem = this.searchForShortestItem(dists)
-
+            let nextItem = this.searchForShortestItem(dists,items);
             let pathToNextItem = [nextItem];
             let curPathNode = nextItem;
-            while(dists[curPathNode].parent !== curItem) {
+            while(dists[curPathNode].parent !== curItem.name) {
                 curPathNode = dists[curPathNode].parent;
                 pathToNextItem.unshift(curPathNode);       
+                console.log("set curPathNode to ",curPathNode);
             }   
 
             Array.prototype.push.apply(path, pathToNextItem)
@@ -39,6 +123,25 @@ class ShortestPathFinder{
         return path;
     }
     dijkstras(node,items){
+        let dists={};
+        dists[node.name]={dist:0,parent:null};
+        let pq= new PriorityQueue();
+        pq.enqueue(node.name,0);
+        while (!pq.isEmpty()){
+            let dq=pq.dequeue();
+            console.log("dq is ", dq);
+            let curr=this.map[dq];
+            for(const n of Object.keys(curr.neighbors)){
+                if(dists[n] === undefined || dists[n].dist > dists[curr.name].dist + curr.neighbors[n])
+                {
+                    dists[n] = {dist: dists[curr.name].dist + curr.neighbors[n],parent:curr.name};
+                    pq.enqueue(n,dists[n].dist);
+                }
+            }
+        }
+        return dists;
+        //in a loop, continually visit the closest unvisited node to root, relaxing all its neighbors.
+
 
     }
     searchForShortestItem(dists,items){
@@ -56,16 +159,5 @@ class ShortestPathFinder{
     }
 }
 let p = new ShortestPathFinder();
-console.log(p);
-/* IDEAS:
-let curr=map["start"];
-let path=[curr];
-while(items.length > 0)
-{
-    dists = dijkstras(curr);//dists["chicken"] = length of shortest path from curr to chicken
-    curr = searchForShortestItem(dists);
-    items.remove(curr);
-    path.append(curr);
-}
-path.append(map["end"]);
-*/
+let ret = p.getShortestPath(["Juices"]);
+console.log(ret);
