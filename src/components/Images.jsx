954496ae7aebe1bc26f4b0b7cfa829ee6d358940
Jessica Liu <jessicaliu@Jessicas-MacBook-Pro.local>
2021-07-02 17:31:01 -0400

    Displayed items from JSON file

---
+++ src/components/Images.jsx
@@ -0,0 +1,85 @@
+import supp from '../assets/Supplements.png';
+import vitamins from '../assets/Vitamins.png';
+import childsupp from '../assets/ChildSupplements.png';
+import baby from '../assets/BabyProducts.png';
+import protein from '../assets/Protein.png';
+import fruitbars from '../assets/FruitBar.png';
+import nutbars from '../assets/NutBar.png';
+import snackbars from '../assets/SnackBar.png';
+import cookies from '../assets/Cookies.png';
+import candy from '../assets/Candy.png';
+import jerky from '../assets/Jerky.png';
+import snacks from '../assets/Snacks.png';
+import popcorn from '../assets/Popcorn.png';
+import chips from '../assets/Chips.png';
+import nuts from '../assets/Nuts.png';
+import crackers from '../assets/Crackers.png';
+import driedfruit from '../assets/DriedFruit.png';
+import soup from '../assets/Soup.png';
+import noodles from '../assets/Noodles.png';
+import rice from '../assets/Rice.png';
+import beans from '../assets/Beans.png';
+import olives from '../assets/Olives.png';
+import sauce from '../assets/Sauce.png';
+import cannedfish from '../assets/CannedFish.png';
+import condiments from '../assets/Condiments.png';
+import flour from '../assets/Flour.png';
+import sugar from '../assets/Sugar.png';
+import spices from '../assets/Spices.png';
+import oil from '../assets/Oil.png';
+import bread from '../assets/Bread.png';
+import cereal from '../assets/Cereal.png';
+import oats from '../assets/Oats.png';
+import syrup from '../assets/Syrup.png';
+import jam from '../assets/Jam.png';
+import pb from '../assets/Pb.png';
+import honey from '../assets/Honey.png';
+import drinks from '../assets/Drinks.png';
+import juices from '../assets/Juices.png';
+import tea from '../assets/Tea.png';
+import coffee from '../assets/Coffee.png';
+
+const images = [
+  { id: 1, src: supp},
+  { id: 2, src: vitamins},
+  { id: 3, src: childsupp},
+  { id: 4, src: baby},
+  { id: 5, src: protein},
+  { id: 6, src: fruitbars},
+  { id: 7, src: nutbars},
+  { id: 8, src: snackbars},
+  { id: 9, src: cookies},
+  { id: 10, src: candy},
+  { id: 11, src: jerky},
+  { id: 12, src: snacks},
+  { id: 13, src: popcorn},
+  { id: 14, src: chips},
+  { id: 15, src: nuts},
+  { id: 16, src: crackers},
+  { id: 17, src: driedfruit},
+  { id: 18, src: soup},
+  { id: 19, src: noodles},
+  { id: 20, src: rice},
+  { id: 21, src: beans},
+  { id: 22, src: olives},
+  { id: 23, src: sauce},
+  { id: 24, src: cannedfish},
+  { id: 25, src: condiments},
+  { id: 26, src: flour},
+  { id: 27, src: sugar},
+  { id: 28, src: spices},
+  { id: 29, src: oil},
+  { id: 30, src: bread},
+  { id: 31, src: cereal},
+  { id: 32, src: oats},
+  { id: 33, src: syrup},
+  { id: 34, src: jam},
+  { id: 35, src: pb},
+  { id: 36, src: honey},
+  { id: 37, src: drinks},
+  { id: 38, src: juices},
+  { id: 39, src: tea},
+  { id: 40, src: coffee},
+];
+
+export default images;
