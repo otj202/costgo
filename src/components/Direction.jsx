20e4a16a586a0f2347354940f9ba4c160a56a69d
Madina Diane <bahmadina19@gmail.com>
2021-07-07 14:56:36 -0400

    Changed direction

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import map from '../data/map.json'
-import styled from 'styled-components'; 
+import styled, { ThemeConsumer } from 'styled-components'; 
 
 
 
@@ -9,10 +9,15 @@ class Direction extends Component {
 
 
     render() {
-
+    var array= [];
+    for(const item of this.props.path){
+        if (map[item].connectorType == null) {
+            array.push(item);
+          }
+    }
       return (
         <div>
-            {this.props.path.map((item)=><h3>{map[item].name} {map[item].aisle}</h3>)}
+            {array.map((item)=><h3>{map[item].name} {map[item].aisle}</h3>)}
         </div>
       );
     }
